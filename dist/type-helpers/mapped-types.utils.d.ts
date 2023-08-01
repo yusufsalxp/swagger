3ea2ece16ebfaeed774ca400af009d47491e477a
Yusuf ŞAL <yusuf.sal.1996@gmail.com>
2023-08-01 10:06:33 +0300

    dist has been removed

--- dist/type-helpers/mapped-types.utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { Type } from '@nestjs/common';
-import { Constructor } from '@automapper/core';
-export declare function clonePluginMetadataFactory(target: Type<unknown>, parent: Type<unknown>, transformFn?: (metadata: Record<string, any>) => Record<string, any>): void;
-export declare function inheritAutoMapMetadata(parentClass: Constructor, targetClass: Function, isPropertyInherited?: (key: string) => boolean): void;
